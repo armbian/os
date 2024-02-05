ed7b3254e66a6835d6f9a3c55bbf33e1ea7637a2
Igor Pecovnik <igor.pecovnik@gmail.com>
2024-02-05 19:16:07 +0100

    Bugfix - fixing HA installation for UEFI targets

--- userpatches/extensions/ha.sh
+++ userpatches/extensions/ha.sh
@@ -38,14 +38,26 @@ function extension_prepare_config__home_assistant() {
 	declare -g HA_OS_AGENT_URL="https://github.com/home-assistant/os-agent/releases/download/${HA_OS_AGENT_VERSION}/${HA_OS_AGENT_FILENAME}"
 	declare -g HA_OS_AGENT_CACHE_FILE="${HA_OS_AGENT_CACHE_DIR}/${HA_OS_AGENT_FILENAME}"
 
+	# Fetch supervised repostory from release 1.6.0, patch it to disable Grub update and install
+	# Without this patch, installation breaks with /usr/sbin/grub-probe: error: failed to get canonical path of `tmpfs'
+	fetch_from_repo "https://github.com/home-assistant/supervised-installer" "supervised-installer" "commit:c99ffd00fcb32c06fc4140040c9ee7e919becce9"
+	cd "${SRC}"/cache/sources/supervised-installer || exit
+
+	# Updating grup fails in chroot and we do it later anyway
+	sed -i "/update-grub/d" homeassistant-supervised/DEBIAN/postinst
+
+	# Build deb file
+	chmod 555 homeassistant-supervised/DEBIAN/p*
+	dpkg-deb -v --build --root-owner-group homeassistant-supervised
+
 	# supervised deb: all
-	declare -g HA_SUPERVISED_VERSION="1.5.0"
+	declare -g HA_SUPERVISED_VERSION="1.6.1"
 	declare -g HA_SUPERVISED_FILENAME="homeassistant-supervised_${HA_SUPERVISED_VERSION}.deb"
 	declare -g HA_SUPERVISED_URL="https://github.com/home-assistant/supervised-installer/releases/download/${HA_SUPERVISED_VERSION}/homeassistant-supervised.deb"
 	declare -g HA_SUPERVISED_CACHE_FILE="${HA_OS_AGENT_CACHE_DIR}/${HA_SUPERVISED_FILENAME}"
 
-	display_alert "Adding HA packages" "${EXTENSION}" "info"
-	add_packages_to_image systemd-journal-remote apparmor
+	display_alert "Adding HA dependency packages" "${EXTENSION}" "info"
+	add_packages_to_image systemd-journal-remote apparmor cifs-utils
 
 	EXTRA_IMAGE_SUFFIXES+=("-homeassistant") # global array
 
@@ -53,6 +65,9 @@ function extension_prepare_config__home_assistant() {
 
 	display_alert "Fetching Home Assistant debs" "${EXTENSION}" "info"
 	mkdir -p "${HA_OS_AGENT_CACHE_DIR}"
+
+	cp homeassistant-supervised.deb "${HA_OS_AGENT_CACHE_DIR}/${HA_SUPERVISED_FILENAME}"
+
 	if [[ -f "${HA_OS_AGENT_CACHE_FILE}" ]]; then
 		display_alert "Using cached Home Assistant os-agent deb" "${HA_OS_AGENT_CACHE_FILE}" "info"
 	else
@@ -78,7 +93,6 @@ function post_install_kernel_debs__add_home_assistant_debs_to_image() {
 		armhf)
 			MACHINE=tinker
 		;;
-# breaks with /usr/sbin/grub-probe: error: failed to get canonical path of `tmpfs'
                 amd64)
 			MACHINE=generic-x86-64
 		;;
@@ -89,11 +103,13 @@ function post_install_kernel_debs__add_home_assistant_debs_to_image() {
 		exit_with_error "Home Assistant Supervised is not supported on ${ARCH} architecture"
 		;;
 	esac
-        # hack os-release to say its Debian and HA won't complain that its unsupported OS
-        sed -i 's/^PRETTY_NAME=".*/PRETTY_NAME="Debian GNU\/Linux 12 (bookworm)"/g' "${SDCARD}/etc/os-release"
 
-        # install HA supervised
-        chroot_sdcard MACHINE=${MACHINE} dpkg -i "/opt/hainstall/${HA_SUPERVISED_FILENAME}"
+	# hack os-release to say its Debian
+	sed -i 's/^PRETTY_NAME=".*/PRETTY_NAME="Debian GNU\/Linux 12 (bookworm)"/g' "${SDCARD}/etc/os-release"
+
+	# install HA supervised
+	chroot_sdcard MACHINE=${MACHINE} dpkg -i "/opt/hainstall/${HA_SUPERVISED_FILENAME}"
+
 }
 
 function image_specific_armbian_env_ready__set_cgroupsv1_in_armbianEnvTxt() {
