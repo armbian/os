d4e96293da4a5f5bd73a2adcea067c6da26450b5
Igor <igor@armbian.com>
2024-03-10 23:12:52 +0100

    Update amazingfated-rk3588.sh

--- userpatches/extensions/amazingfated-rk3588.sh
+++ userpatches/extensions/amazingfated-rk3588.sh
@@ -5,34 +5,29 @@
 #
 
 # This add's amazingfate's PPAs to the the image, and installs all needed packages.
-# It only works on LINUXFAMILY="rk3588-legacy" and RELEASE=jammy and BRANCH=legacy/vendor
+# It only works on LINUXFAMILY="rk3588-legacy" and RELEASE=jammy and BRANCH=legacy
 # if on a desktop, installs more useful packages, and tries to coerce lightdm to use gtk-greeter and a Wayland session.
 function extension_prepare_config__amazingfated_rk3588() {
-
-	display_alert "Preparing amazingfated's rk3588 extension" "${EXTENSION}" "info"
-	# Add to the image suffix.
-	EXTRA_IMAGE_SUFFIXES+=("-amazingfated") # global array
-
-	[[ "${BUILDING_IMAGE}" != "yes" ]] && return 0
 
 	if [[ "${LINUXFAMILY}" != "rockchip-rk3588" && "${LINUXFAMILY}" != "rk35xx" ]]; then
 		exit_with_error "${EXTENSION} only works on LINUXFAMILY=rockchip-rk3588/rk35xx, currently on '${LINUXFAMILY}'"
 	fi
 
-	if [[ "${BRANCH}" != "legacy" && "${BRANCH}" != "vendor" ]]; then
-		exit_with_error "${EXTENSION} only works on BRANCH=legacy or BRANCH=vendor, currently on '${BRANCH}'"
-	fi
+        [[ "${BUILDING_IMAGE}" != "yes" ]] && return 0
+        [[ "${RELEASE}" != "jammy" ]] && return 0
+	[[ "${BRANCH}" != "legacy" && "${BRANCH}" != "vendor" ]] && return 0
+        [[ "${BUILD_DESKTOP}" != "yes" ]] && return 0
 
-	if [[ "${RELEASE}" != "jammy" ]]; then
-		exit_with_error "${EXTENSION} only works on RELEASE=jammy, currently on '${RELEASE}'"
-	fi
+	display_alert "Preparing amazingfated's rk3588 extension" "${EXTENSION}" "info"
+	# Add to the image suffix.
+	EXTRA_IMAGE_SUFFIXES+=("-amazingfated") # global array
 
 }
 
 function post_install_kernel_debs__amazingfated_rk358() {
 
         [[ "${RELEASE}" != "jammy" ]] && return 0
-	[[ "${BRANCH}" != "legacy" ]] && return 0
+	[[ "${BRANCH}" != "legacy" && "${BRANCH}" != "vendor" ]] && return 0
         [[ "${BUILD_DESKTOP}" != "yes" ]] && return 0
 
 	display_alert "Adding amazingfated's rk3588 PPAs" "${EXTENSION}" "info"
@@ -54,4 +49,3 @@ function post_install_kernel_debs__amazingfated_rk358() {
 
 	return 0
 }
-
