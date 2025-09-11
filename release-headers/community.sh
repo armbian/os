806848392e08f4bdea871c692f78ed2fbdf6e371
Igor Pecovnik <igor@armbian.com>
2025-09-11 15:49:19 +0200

    Make release headers dynamicall with a bash script to keep svg files refreshing.

---
+++ release-headers/community.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TS=$(date +%s)
+
+cat <<EOF
+<p align='center'>
+  <a href="https://www.armbian.com"><img src="https://raw.githubusercontent.com/armbian/.github/master/profile/logosmall.png" alt="Armbian logo"></a>
+  <h1 align=center>Community releases</h1>
+</p>
+
+<p align='center'>
+  <a href="https://www.armbian.com"><img
+      src="https://github.armbian.com/current.svg?$TS"
+      alt="Armbian Linux CURRENT"
+      width="425"></a> &nbsp; 
+  <a href="https://www.armbian.com"><img
+      src="https://github.armbian.com/edge.svg?$TS"
+      alt="Armbian Linux EDGE"
+      width="425"></a>
+</p>
+
+<p align='center'>
+  Community courtesy builds are not maintained by Armbian team, but exist thanks to 
+  <a href="https://github.com/sponsors/armbian">community support</a>.
+</p>
+EOF
---
+++ release-headers/os.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TS=$(date +%s)
+
+cat <<EOF
+<p align='center'>
+  <a href="https://www.armbian.com"><img src="https://raw.githubusercontent.com/armbian/.github/master/profile/logosmall.png" alt="Armbian logo"></a>
+  <h1 align=center>Rolling releases</h1>
+</p>
+
+<p align='center'>
+  <a href="https://www.armbian.com"><img
+      src="https://github.armbian.com/current.svg?$TS"
+      alt="Armbian Linux CURRENT"
+      width="425"></a> &nbsp; 
+  <a href="https://www.armbian.com"><img
+      src="https://github.armbian.com/edge.svg?$TS"
+      alt="Armbian Linux EDGE"
+      width="425"></a>
+</p>
+
+<p align='center'>
+  Rolling releases are daily builds with the latest kernel and userspace packages for supported hardware.
+</p>
+EOF
